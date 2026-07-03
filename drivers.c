3467fca8f0812cb23c7e6807330b385d481518ae
SpaceiFix <celeronsurvival@gmail.com>
2026-07-04 00:03:00 +0300

    normal keyboard layout
    normal keyboard layout

--- drivers.c
+++ drivers.c
@@ -1,5 +1,6 @@
 #include "drivers.h"
 
+#define MOD_SHIFT 1
 #define VIDEO_ADDRESS 0xB8000
 #define MAX_ROWS 25
 #define MAX_COLS 80
@@ -8,12 +9,60 @@
 static int cursor_row = 0;
 static int cursor_col = 0;
 
-static const char ascii_map[] = {
-    0,  27, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\b',
-    '\t', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',
-    0,  'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '`', '\'',  0,
-    '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/',   0,   '*',   0, ' '
+static unsigned char key_modifiers = 0;
+
+struct KeyEntry {
+    char normal;
+    char shift;
 };
+
+static const struct KeyEntry keymap[] = {
+    [0x00] = {0, 0},     [0x01] = {27, 27},   [0x02] = {'1', '!'}, [0x03] = {'2', '@'},
+    [0x04] = {'3', '#'}, [0x05] = {'4', '$'}, [0x06] = {'5', '%'}, [0x07] = {'6', '^'},
+    [0x08] = {'7', '&'}, [0x09] = {'8', '*'}, [0x0A] = {'9', '?'}, [0x0B] = {'0', ')'},
+    [0x0C] = {'-', '_'}, [0x0D] = {'=', '+'}, [0x0E] = {'\b', '\b'},[0x0F] = {'\t', '\t'},
+    [0x10] = {'q', 'Q'}, [0x11] = {'w', 'W'}, [0x12] = {'e', 'E'}, [0x13] = {'r', 'R'},
+    [0x14] = {'t', 'T'}, [0x15] = {'y', 'Y'}, [0x16] = {'u', 'U'}, [0x17] = {'i', 'I'},
+    [0x18] = {'o', 'O'}, [0x19] = {'p', 'P'}, [0x1A] = {'[', '{'}, [0x1B] = {']', '}'},
+    [0x1C] = {'\n', '\n'},[0x1D] = {0, 0},     [0x1E] = {'a', 'A'}, [0x1F] = {'s', 'S'},
+    [0x20] = {'d', 'D'}, [0x21] = {'f', 'F'}, [0x22] = {'g', 'G'}, [0x23] = {'h', 'H'},
+    [0x24] = {'j', 'J'}, [0x25] = {'k', 'K'}, [0x26] = {'l', 'L'}, [0x27] = {';', ':'},
+    [0x28] = {'\'', '"'}, [0x29] = {'`', '~'}, [0x2A] = {0, 0},     [0x2B] = {'\\', '|'},
+    [0x2C] = {'z', 'Z'}, [0x2D] = {'x', 'X'}, [0x2E] = {'c', 'C'}, [0x2F] = {'v', 'V'},
+    [0x30] = {'b', 'B'}, [0x31] = {'n', 'N'}, [0x32] = {'m', 'M'}, [0x33] = {',', '<'},
+    [0x34] = {'.', '>'}, [0x35] = {'/', '?'}, [0x36] = {0, 0},     [0x39] = {' ', ' '}
+}; // лучшая таблица символов 2.0
+
+char get_char(void) {
+    while ((port_byte_in(0x64) & 1) == 0);
+    unsigned char scancode = port_byte_in(0x60);
+
+    if (scancode == 0x2A || scancode == 0x36) {
+        key_modifiers |= MOD_SHIFT;
+        return 0;
+    }
+
+    if (scancode == 0x2A + 0x80 || scancode == 0x36 + 0x80) {
+        key_modifiers &= ~MOD_SHIFT;
+        return 0;
+    }
+
+    if (scancode >= 0x80) {
+        return 0;
+    }
+
+    if (scancode < sizeof(keymap) / sizeof(struct KeyEntry)) {
+        struct KeyEntry entry = keymap[scancode];
+
+        if (key_modifiers & MOD_SHIFT) {
+            return entry.shift;
+        }
+        return entry.normal;
+    }
+
+    return 0;
+}
+
 
 unsigned char port_byte_in(unsigned short port) {
     unsigned char result;
@@ -148,13 +197,4 @@ void kprint(const char *str) {
     while (*str) {
         kprint_char(*str++);
     }
-}
-
-char get_char() {
-    while ((port_byte_in(0x64) & 1) == 0);
-    unsigned char scancode = port_byte_in(0x60);
-    if (scancode < sizeof(ascii_map)) {
-        return ascii_map[scancode];
-    }
-    return 0;
 }
